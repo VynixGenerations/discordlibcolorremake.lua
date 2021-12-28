a9858dc2d0f08fc50f760e9bba88ee7315a87770
VynixGenerations <96760529+VynixGenerations@users.noreply.github.com>
2021-12-28 00:19:02 -0500

    Update LibColorRemake.lua

--- LibColorRemake.lua
+++ LibColorRemake.lua
@@ -1569,7 +1569,7 @@ function DiscordLib:Window(text)
 
 		UnderBar.Name = "UnderBar"
 		UnderBar.Parent = Notification
-		UnderBar.BackgroundColor3 = Color3.fromRGB(22, 29, 50)
+		UnderBar.BackgroundColor3 = Color3.fromRGB(98, 37, 209)
 		UnderBar.Position = UDim2.new(-0.000297061284, 0, 0.945048928, 0)
 		UnderBar.Size = UDim2.new(0, 346, 0, 10)
 
