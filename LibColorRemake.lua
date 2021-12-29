c500938a06d4c0ac8a8b8a73d1c8af31dc4260c8
VynixGenerations <96760529+VynixGenerations@users.noreply.github.com>
2021-12-29 14:53:01 -0500

    Update LibColorRemake.lua

--- LibColorRemake.lua
+++ LibColorRemake.lua
@@ -2147,7 +2147,7 @@ function DiscordLib:Window(text)
 					TweenService:Create(
 						Button,
 						TweenInfo.new(.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
-						{BackgroundColor3 = Color3.fromRGB(103,123,196)}
+						{BackgroundColor3 = Color3.fromRGB(78, 0, 121)}
 					):Play()
 				end)
 				
@@ -2165,7 +2165,7 @@ function DiscordLib:Window(text)
 					TweenService:Create(
 						Button,
 						TweenInfo.new(.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
-						{BackgroundColor3 = Color3.fromRGB(78, 0, 121)}
+						{BackgroundColor3 = Color3.fromRGB(30,30,30)}
 					):Play()
 				end)
 				ChannelHolder.CanvasSize = UDim2.new(0,0,0,ChannelHolderLayout.AbsoluteContentSize.Y)
