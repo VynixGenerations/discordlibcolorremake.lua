70fb8797fe754a23efeaf80a30b03113bc9be705
VynixGenerations <96760529+VynixGenerations@users.noreply.github.com>
2021-12-29 19:32:50 -0500

    Update LibColorRemake.lua

--- LibColorRemake.lua
+++ LibColorRemake.lua
@@ -1814,7 +1814,7 @@ function DiscordLib:Window(text)
 		Hashtag.Position = UDim2.new(0.0279720277, 0, 0, 0)
 		Hashtag.Size = UDim2.new(0, 19, 0, 39)
 		Hashtag.Font = Enum.Font.Gotham
-		Hashtag.Text = "*"
+		Hashtag.Text = "#"
 		Hashtag.TextColor3 = Color3.fromRGB(114, 118, 125)
 		Hashtag.TextSize = 25.000
 
@@ -3149,7 +3149,7 @@ function DiscordLib:Window(text)
 
 				Label.Name = "Label"
 				Label.Parent = ChannelHolder
-				Label.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+				Label.BackgroundColor3 = Color3.fromRGB(41, 43, 47)
 				Label.BorderSizePixel = 0
 				Label.Position = UDim2.new(0.261979163, 0, 0.190789461, 0)
 				Label.Size = UDim2.new(0, 401, 0, 30)
