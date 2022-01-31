849d2ce4664c70b52227c6a9487521a32b0618cb
VynixGenerations <96760529+VynixGenerations@users.noreply.github.com>
2022-01-31 15:50:30 -0500

    Update otherlib.lua

--- otherlib.lua
+++ otherlib.lua
@@ -11,19 +11,18 @@ local tag
 local userinfo = {}
 
 local themes = {
-	Light = {
-		MainFrame.BackgroundColor3 = Color3.fromRGB(250, 250, 250),
+    JapanTheme = {
+        MainFrame.BackgroundColor3 = Color3.fromRGB(250, 250, 250),
 		TopFrame.BackgroundColor3 = Color3.fromRGB(203, 203, 203),
 		TopFrameHolder.BackgroundColor3 = Color3.fromRGB(203, 203, 203),
 		ServersHold.BackgroundColor3 = Color3.fromRGB(208, 208, 208),
 		HashtagLabel.Text = "",
 		ChannelBtnHashtag.Text = "",
 		NotificationHolderMain.BackgroundColor3 = Color3.fromRGB(213, 213, 213)
-
-
-
-	}
+    }
 }
+
+
 
 
 
